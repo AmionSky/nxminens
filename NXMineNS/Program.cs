29694546d1316f5dea8cb0aa72729af4c0541905
Csányi István <csanyistvan02@gmail.com>
2021-05-17 11:28:26 +0200

    update

--- NXMineNS/Program.cs
+++ NXMineNS/Program.cs
@@ -22,21 +22,22 @@ namespace NXMineNS
                 try
                 {
                     startServer(args[0]).Wait();
-                    
-                }catch(Exception ex)
+
+                }
+                catch (Exception ex)
                 {
                     Console.WriteLine("========EXCEPTION=======");
                     Console.WriteLine(ex.ToString());
                     Console.WriteLine("========================");
                 }
-                
+
             }
-            
+
         }
         static async Task startServer(string ip)
         {
             Console.WriteLine("Starting Nameserver to redirect all known Minecraft \"Featured Servers\" to " + ip);
-            
+
             MasterFile masterFile = new MasterFile();
             DnsServer server = new DnsServer(masterFile, "8.8.8.8");
 
@@ -49,16 +50,22 @@ namespace NXMineNS
             masterFile.AddIPAddressResourceRecord("play.galaxite.net", ip);
 
             // Log every request
-            server.Requested += (sender, e) => Console.WriteLine(e.Request);
+            server.Requested += (sender, e) =>
+            {
+                if (e.Request.Questions.Count > 0)
+                {
+                    Console.WriteLine(e.Request.Questions[0].Name);
+                }
+            };
             // On every successful request log the request and the response
-            server.Responded += (sender, e) => Console.WriteLine("{0} => {1}", e.Request, e.Response);
+            //server.Responded += (sender, e) => Console.WriteLine("{0} => {1}", e.Request, e.Response);
             // Log errors
             server.Errored += (sender, e) => Console.WriteLine(e.Exception.Message);
 
             // Start the server (by default it listents on port 53)
             Console.WriteLine("Looks like we are all set. Set your Switch's DNS to the IP of this computer!");
             await server.Listen();
-            
+
         }
     }
 }
